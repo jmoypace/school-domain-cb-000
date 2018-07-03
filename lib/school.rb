41cae59a311232a29cd492756f6b702351109d4f
jmoypace <jmoypace@gmail.com>
2018-07-03 02:22:01 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,7 @@ class School
       roster[grade] << student_name
     end
   # this method should arrange the students in each grade by alphabetical order
-  def grade(student_grade)
-      roster[student_grade]
+  def grade(grade)
+      roster[grade]
     end
 end
