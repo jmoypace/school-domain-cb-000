07c99ca331a6827414e2ea92983f6a438e228502
jmoypace <jmoypace@gmail.com>
2018-07-03 02:11:38 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,11 +2,10 @@
 class School
 attr_accessor :name, :roaster
 
-def initialized(name)
-  @name = name
-  @roaster = {}
-
-end
+def initialize(name)
+    @name = name
+    @roster = {}
+  end
 
 def add_student=(student_name, grade)
 
