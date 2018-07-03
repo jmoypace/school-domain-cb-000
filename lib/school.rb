be2784f2ffe9f37c8fcf443d9f5a1cb3ac6023a7
jmoypace <jmoypace@gmail.com>
2018-07-03 02:25:54 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,4 +14,12 @@ class School
   def grade(grade)
       roster[grade]
     end
+
+    def sort
+      sorted = {}
+      roster.each do |grade, students|
+        sorted[grade] = students.sort
+      end
+      sorted
+    end
 end
