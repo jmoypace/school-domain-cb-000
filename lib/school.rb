49b97d2c0b2ecaf3ed9b43f85facd19573c27ddb
jmoypace <jmoypace@gmail.com>
2018-07-03 02:43:35 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
   end
 
   def add_student(student_name, grade)
-      @roster[grade] = []
+      @roster[grade] ||= []
       @roster[grade] << student_name
     end
   # this method should arrange the students in each grade by alphabetical order
